3b73a239c75cf388740a86deb84493d2380f2989
Paul Applegate <paul@theapplegates.com>
2022-10-03 22:01:39 -0400

    first commit

---
+++ astro.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+import sharp from '@astrojs/image/sharp';
+
+export default defineConfig({
+	markdown: {
+		shikiConfig: {
+			theme: "dracula",
+			wrap: true,
+		},
+	},
+	site: "https://www.astro-theme-cactus.netlify.app",
+	integrations: [
+		mdx({}),
+		tailwind({
+			config: { applyBaseStyles: false },
+		}),
+		image({
+			serviceEntryPoint: '@astrojs/image/sharp',
+		  }),
+		sitemap(),
+	],
+});
